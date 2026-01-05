021710c45ecfa077e31e2b0839fd3d650391b182
Sonali2824 <sonali.24112@gmail.com>
2026-01-05 11:09:11 +0000

    Project Structure Added

---
+++ gateway-service/src/main/java/com/sonali/ai/GatewayServiceApplication.java
@@ -0,0 +1,11 @@
+package com.sonali.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
